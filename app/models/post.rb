412981c15fdb4a4c1e9da03240e544f7d6b8781a
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-19 03:18:42 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,18 +3,19 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(fiction non-fiction)}
-  validate :validate
+  validate :validated_clickbait
 
-  def validate
-    clickbait = ["Won't Believe", "Secret", "Guess"]
-  truthy = false
-  if self.title && !self.title.scan(/Top\w[0-9]/).empty?
-      truthy = true
-  elsif self.title
-      truthy = clickbait.any? do |bait|
-          self.title.include?(bait)
-      end
-  end
-  errors.add(:title, "must be clickbaity") unless truthy
+  CLICKBAIT = ["Won't Believe", "Secret", "Top", "Guess"]
+
+  def validated_clickbait
+    if self.title == nil
+      return false
+    end
+    if self.title.include?(CLICKBAIT[0]) || self.title.include?(CLICKBAIT[1]) || self.title.include?(CLICKBAIT[2]) || self.title.include?(CLICKBAIT[3])
+      return true
+    else
+      errors.add(:title, "Must be clickbait-y")
+    return false
+    end
   end
 end
