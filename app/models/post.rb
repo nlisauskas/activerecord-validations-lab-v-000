ddc74608788ab7f70fe8b47b4a8ac0d41ce38f94
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-19 03:19:42 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :validated_clickbait
+
+  CLICKBAIT = ["Won't Believe", "Secret", "Top", "Guess"]
+
+  def validated_clickbait
+    if self.title == nil
+      return false
+    end
+    if self.title.include?(CLICKBAIT[0]) || self.title.include?(CLICKBAIT[1]) || self.title.include?(CLICKBAIT[2]) || self.title.include?(CLICKBAIT[3])
+      return true
+    else
+      errors.add(:title, "Must be clickbait-y")
+    return false
+    end
+  end
 end
